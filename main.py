69a042309db55a4654fb81e6765a4a82402b4a5f
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2025-01-28 23:58:12 +0300

    Refactor game loop to use asyncio for improved performance and responsiveness

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from random import randrange
 
-import pygame
+import pygame, asyncio
 from pygame import Vector2
 
 import config
 
 screen = pygame.display.set_mode((config.SCREEN_SIZE, config.SCREEN_SIZE))
 clock = pygame.time.Clock()
-
-running = True
-begin = True
-bait = True
 
 time = None
 snake_rect = None
@@ -20,70 +16,74 @@ snake_direction = None
 
 food_rect = None
 
-while running:
-    if begin:
-        begin = False
-        time = 0
-        snake_rect = pygame.rect.Rect(
-            [randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
-             randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
-             config.SNAKE_PART_SIZE,
-             config.SNAKE_PART_SIZE])
-        snake_length = 1
-        snake_parts = []
-        snake_direction = Vector2(0, 0)
+async def main():
+    begin = True
+    bait = True
 
-    if bait:
-        bait = False
-        food_rect = pygame.rect.Rect([randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
-                                      randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
-                                      config.FOOD_SIZE,
-                                      config.FOOD_SIZE])
+    while True:
+        if begin:
+            begin = False
+            time = 0
+            snake_rect = pygame.rect.Rect(
+                [randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
+                randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
+                config.SNAKE_PART_SIZE,
+                config.SNAKE_PART_SIZE])
+            snake_length = 1
+            snake_parts = []
+            snake_direction = Vector2(0, 0)
 
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT or
-                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and not snake_direction[1] > 0:
-                snake_direction = Vector2(0, -config.SNAKE_MOVE_LENGTH)
-            if event.key == pygame.K_DOWN and not snake_direction[1] < 0:
-                snake_direction = Vector2(0, config.SNAKE_MOVE_LENGTH)
-            if event.key == pygame.K_LEFT and not snake_direction[0] > 0:
-                snake_direction = Vector2(-config.SNAKE_MOVE_LENGTH, 0)
-            if event.key == pygame.K_RIGHT and not snake_direction[0] < 0:
-                snake_direction = Vector2(config.SNAKE_MOVE_LENGTH, 0)
+        if bait:
+            bait = False
+            food_rect = pygame.rect.Rect([randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
+                                        randrange(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE),
+                                        config.FOOD_SIZE,
+                                        config.FOOD_SIZE])
 
-    time_now = pygame.time.get_ticks()
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT or
+                    (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and not snake_direction[1] > 0:
+                    snake_direction = Vector2(0, -config.SNAKE_MOVE_LENGTH)
+                if event.key == pygame.K_DOWN and not snake_direction[1] < 0:
+                    snake_direction = Vector2(0, config.SNAKE_MOVE_LENGTH)
+                if event.key == pygame.K_LEFT and not snake_direction[0] > 0:
+                    snake_direction = Vector2(-config.SNAKE_MOVE_LENGTH, 0)
+                if event.key == pygame.K_RIGHT and not snake_direction[0] < 0:
+                    snake_direction = Vector2(config.SNAKE_MOVE_LENGTH, 0)
 
-    screen.fill(config.BG_COLOR)
+        time_now = pygame.time.get_ticks()
 
-    for i in range(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE):
-        pygame.draw.line(screen, config.GRID_COLOR, (i, 0), (i, config.SCREEN_SIZE))
-        pygame.draw.line(screen, config.GRID_COLOR, (0, i), (config.SCREEN_SIZE, i))
+        screen.fill(config.BG_COLOR)
 
-    if time_now - time > config.DELAY:
-        time = time_now
-        snake_rect.move_ip(snake_direction)
-        snake_parts.append(snake_rect.copy())
-        snake_parts = snake_parts[-snake_length:]
+        for i in range(0, config.SCREEN_SIZE, config.GRID_CELL_SIZE):
+            pygame.draw.line(screen, config.GRID_COLOR, (i, 0), (i, config.SCREEN_SIZE))
+            pygame.draw.line(screen, config.GRID_COLOR, (0, i), (config.SCREEN_SIZE, i))
 
-    pygame.draw.rect(screen, config.FOOD_COLOR, food_rect, 0, 10)
+        if time_now - time > config.DELAY:
+            time = time_now
+            snake_rect.move_ip(snake_direction)
+            snake_parts.append(snake_rect.copy())
+            snake_parts = snake_parts[-snake_length:]
 
-    [pygame.draw.rect(screen, config.SNAKE_COLOR, snake_part, 8, 4)
-     for snake_part in snake_parts]
+        pygame.draw.rect(screen, config.FOOD_COLOR, food_rect, 0, 10)
 
-    if (snake_rect.left < 0 or snake_rect.right > config.SCREEN_SIZE or
-            snake_rect.top < 0 or snake_rect.bottom > config.SCREEN_SIZE or
-            len(snake_parts) != len(set(snake_part.center for snake_part in snake_parts))):
-        begin = True
+        [pygame.draw.rect(screen, config.SNAKE_COLOR, snake_part, 8, 4)
+        for snake_part in snake_parts]
 
-    if snake_rect.center == food_rect.center:
-        snake_length += 1
-        bait = True
+        if (snake_rect.left < 0 or snake_rect.right > config.SCREEN_SIZE or
+                snake_rect.top < 0 or snake_rect.bottom > config.SCREEN_SIZE or
+                len(snake_parts) != len(set(snake_part.center for snake_part in snake_parts))):
+            begin = True
 
-    pygame.display.flip()
+        if snake_rect.center == food_rect.center:
+            snake_length += 1
+            bait = True
 
-    clock.tick(config.FPS)
+        pygame.display.flip()
+        clock.tick(config.FPS)
+        await asyncio.sleep(0)
 
-pygame.quit()
+asyncio.run(main())
